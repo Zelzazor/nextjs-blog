e9138e9175ad34b0bbc87003ba015b3f3a878cf9
Felipe Ramírez <zelzazor.ramirez62@gmail.com>
2022-02-08 17:41:45 -0400

    Created blog

---
+++ lib/interface.ts
@@ -0,0 +1,6 @@
+export interface IBlogInfo {
+  id: string;
+  date: string;
+  title: string;
+  contentHtml?: string;
+}
---
+++ pages/posts/[id].tsx
@@ -0,0 +1,42 @@
+import Head from "next/head";
+import Layout from "../../components/layout";
+import { IBlogInfo } from "../../lib/interface";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css"
+
+export default function Post(props: { postData: IBlogInfo }) {
+  const { postData } = props;
+  return (
+    <Layout home={false}>
+      <Head>
+        <title>{`My blog - ${postData.title}`}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }: any) {
+  const postData = await getPostData(params.id);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+}
